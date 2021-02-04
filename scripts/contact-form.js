e190b0b8bde2ee2887801da63da7cb936ff5e362
Paweł Pilarczyk <56913394+Pawel-Pilarczyk@users.noreply.github.com>
2021-02-04 21:50:32 +0100

    v0.13

---
+++ scripts/contact-form.js
@@ -0,0 +1,27 @@
+// Function that handles clicking on the e-mail from the form
+function sendMeEmail(){
+    window.open('mailto:pawelpilarczyk1@gmail.com');
+}
+// Function that handles sending an e-mail from the form
+function sendEmailFromForm(e){
+    e.preventDefault();
+    const email = document.getElementById('e-mail').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+    document.getElementById('e-mail').value = "";
+    document.getElementById('phone').value = "";
+    document.getElementById('message').value = "";
+    window.open(`mailto:pawelpilarczyk1@gmail.com?subject=Website_Contact&body=FROM:%0D%0A_MAIL_${email}%0D%0A_PHONE_${phone}__%0D%0AMESSAGE_${message}`);
+}
+
+
+const form = document.querySelector('#contact-form');
+const emailLink = document.getElementById('email-shortcut');
+
+form.addEventListener('submit', function(e){
+    sendEmailFromForm(e);
+});
+
+emailLink.addEventListener('click',function(e){
+    sendMeEmail();
+})
